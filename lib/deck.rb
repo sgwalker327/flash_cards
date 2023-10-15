b10d0a634bc6a37269ed0d677e8fde041d43067c
Sam Walker <sgwalker327@gmail.com>
2023-10-15 17:02:51 -0600

    Create count and cards_in_cateory methods for Deck, still need to write tests.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,7 +1,17 @@
 class Deck
   attr_reader :cards
 
-  def initialize
-    @cards = 
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def count
+    @cards.count
+  end
+
+  def cards_in_category(cat)
+    @cards.find_all do |card|
+      card.category == cat
+    end
   end
 end
